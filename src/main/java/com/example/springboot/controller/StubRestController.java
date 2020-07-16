5cb9937a624ca4aba35b96b118d7724b1da011ca
tkhr-ssk <tkhr.ssk@gmail.com>
2020-07-17 00:57:28 +0900

    modify typo

--- src/main/java/com/example/springboot/controller/StubRestController.java
+++ src/main/java/com/example/springboot/controller/StubRestController.java
@@ -1,4 +1,4 @@
-package com.example.springboot.contoller;
+package com.example.springboot.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
